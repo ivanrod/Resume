6d799967fcd32b6989294515bfecba835890118e
Iván Rodriguez <ivan.rodriguez@bq.com>
2016-10-25 22:57:05 +0200

    Add assets

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,22 @@
-var gulp = require('gulp');
-var resume = require('gulp-resume');
-var rename = require('gulp-rename');
+const gulp = require('gulp');
+const resume = require('gulp-resume');
+const rename = require('gulp-rename');
 
-gulp.task('resume', function() {
+function copyAssets () {
+  return gulp.src('./assets/**')
+     .pipe(gulp.dest('./dist/assets/'));
+};
+
+function makeResume() {
   return gulp.src('resume.json')
     .pipe(resume({
       format: 'html',
       theme: 'elegant'
     }))
     .pipe(rename('resume1.html'))
-    .pipe(gulp.dest('./out'));
-});
+    .pipe(gulp.dest('./dist'));
+};
+
+gulp.task('makeResume', makeResume);
+gulp.task('copyAssets', copyAssets);
+gulp.task('resume', ['makeResume', 'copyAssets']);
